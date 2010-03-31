0ad43050f448c8afb36c146a2e1313e27c1f9161
Andy Schmidt <andy.schmidt@citrusbyte.com>
2010-03-31 01:16:12 -0700

    cap config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,10 @@
+default_run_options[:pty] = true
+ssh_options[:paranoid] = false
 set :application, "kernohan_vocals"
 set :repository,  "git@github.com:bentreflection/kernohan_rails.git"
 
 set :scm, :git
+set :scm_passphrase, ""
 
 set :deploy_to, "~/#{application}"
 
@@ -18,4 +21,12 @@ namespace :deploy do
       run "ln -nfs #{shared_path}/config/#{filename} #{release_path}/config/#{filename}"
     end
   end
+end
+
+
+desc "Restart the web server. Overrides the default task for Site5 use."
+task :restart, :roles => :app do
+  #run "killall -q dispatch.fcgi"
+  run "chmod 755 #{current_path}/public/dispatch.fcgi"
+  run "touch #{current_path}/public/dispatch.fcgi"
 end
