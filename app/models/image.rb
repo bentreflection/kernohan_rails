9586ee07b9eb92a4f7f958923da69638f2a0b7b3
Andy Schmidt <andy.schmidt@citrusbyte.com>
2010-03-31 22:42:08 -0700

    move milton assets to public/assets

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,3 +1,4 @@
 class Image < ActiveRecord::Base
-  is_attachment :recipes => { :thumb => [{ :thumbnail => { :size => '198x132'} }] }
+  is_attachment :recipes => { :thumb => [{ :thumbnail => { :size => '198x132'} }] }, 
+                :storage_options => {:root => File.join(Rails.root, "public", "assets", table_name)}
 end
--- config/deploy.rb
+++ config/deploy.rb
@@ -30,10 +30,33 @@ namespace :deploy do
   end
   
   task :symlink_htaccess, :role => :app do
-    p "HERE IS THE SHARED PATH #{shared_path}"
-    p "HERE IS THE RELEASE PATH #{release_path}"
     run "ln -s #{shared_path}/.htaccess #{release_path}/public/.htaccess"
   end
+  
+  desc "Symlink persisted directories"
+  task :symlink_dirs, :roles => :app, :except => { :no_symlink => true } do
+    # this is a little weird, we can't just link public/images because that's
+    # in the repo, but the Image model is also a dynamic upload path, so we're
+    # pre-linking a bunch of possible dynamic folder destinations, add more if
+    # necessary...
+    %w( tmp/milton public/assets).each do |directory|
+      run "mkdir -p #{shared_path}/#{directory}"
+      run "ln -nfs #{shared_path}/#{directory} #{release_path}/#{directory}"
+    end
+  end  
+  
+  desc "Update CSS files and replace relative urls with absolute ones"
+  task :compile_sass, :roles => :app do
+    haml_dir = 'haml'
+    run "ls #{current_path}/vendor/gems | grep haml" do |channel, stream, data|
+      haml_dir = data
+    end
+    run <<-BASH
+      for file in #{current_path}/public/stylesheets/sass/*.sass; do
+        #{current_path}/vendor/gems/#{haml_dir}/bin/sass $file #{current_path}/public/stylesheets/`basename ${file%.sass}`.css ; done
+    BASH
+  end
+  
   
 end
 
